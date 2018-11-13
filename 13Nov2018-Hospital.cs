50d0f5dd64a49615d78e9a71a159d11af314da77
shahenashaik11 <44662746+shahenashaik11@users.noreply.github.com>
2018-11-12 22:43:15 -0800

    Add files via upload

---
+++ 13Nov2018-Hospital.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+
+    class Person
+    {
+        public string name;
+
+    }
+    class Doctor : Person
+    {
+        public string specialization;
+
+    }
+    class Patient : Person
+    {
+        public int age, fees;
+        public Treatment[] t = new Treatment[3];
+
+    }
+    class Hospital
+    {
+        public Patient[] p = new Patient[3];
+        public Doctor[] d = new Doctor[3];
+
+    }
+    class Treatment
+    {
+        public string diagnosis;
+        public int diagnosiscode;
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hospital h = new Hospital();
+            Patient[] p = new Patient[3];
+            Doctor[] d = new Doctor[3];
+            Treatment[] t = new Treatment[3];
+
+        }
+    }
+}
